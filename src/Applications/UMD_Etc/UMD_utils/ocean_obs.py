e6deeea5d3e64d1d388936ff262f5c60c4788f46
vrx- <vrx000@gmail.com>
2025-03-05 14:46:19 -0500

    updating obs min_req

--- src/Applications/UMD_Etc/UMD_utils/ocean_obs.py
+++ src/Applications/UMD_Etc/UMD_utils/ocean_obs.py
@@ -1296,11 +1296,11 @@ if list_of_obs:
 
 #   check to make sure minimum observation types are in gmao- file
 #   instrument check (i.e. ARGO) follows below
-#   ominreq=[3073, 5521, 5351]  #Tz, Sz, ADT
-    ominreq=[3073, 5351]  #Tz, ADT
+    ominreq=[3073, 5521, 5351]  #Tz, Sz, ADT
+#   ominreq=[3073, 5351]  #Tz, ADT
 #   ominreq=[3073]  #Tz,
 #   ominreq[3073, 5521]  #Tz, Sz
-#    ominreq=[5351]  #ADT
+#   ominreq=[5351]  #ADT
 
     for oid in ominreq:
         if oid in typ:
